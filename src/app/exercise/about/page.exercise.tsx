605a41aed390f58bfcf0d186bb88b877cea4f8fb
theoxav <theoxxav@outlook.fr>
2026-08-27 23:55:03 +0200

    refactor: enable client-side rendering and enhance interactivity

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,12 +1,14 @@
+'use client'
+
 const Page = () => {
   // 🐶 Détermine dans quel environnement s'exécute ce composant grâce à un log
-  // 🤖 `console.log('Je suis un développeur FullStack Next')`
+  console.log('Je suis un développeur FullStack Next')
   //
   // 🐶 Vérifie dans la console du navigateur et dans la console du serveur (terminal)
 
   // 🐶 N'oublie pas de faire les exercices bonus
   return (
-    <div>
+    <div onClick={() => alert('Hello')}>
       <p>Je suis un développeur FullStack Next</p>
     </div>
   )
